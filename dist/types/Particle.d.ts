34da30d0c82278a5102eb4acc8952d017eb3f821
Rygor Kharytanovich <rygor@monterosa.co.uk>
2020-07-21 19:34:56 +0300

    Release

---
+++ dist/types/Particle.d.ts
@@ -0,0 +1,32 @@
+import { IConfettiOptions } from './Confetti';
+export declare enum ParticleShape {
+    Circle = 0,
+    Square = 1,
+    Strip = 2
+}
+declare enum RotationDirection {
+    Positive = 1,
+    Negative = -1
+}
+export default class Particle {
+    constructor(context: CanvasRenderingContext2D, getOptions: () => IConfettiOptions, x: number, y: number);
+    context: CanvasRenderingContext2D;
+    radius: number;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+    vx: number;
+    vy: number;
+    shape: ParticleShape;
+    angle: number;
+    angularSpin: number;
+    color: string;
+    image?: HTMLImageElement;
+    rotateY: number;
+    rotationDirection: RotationDirection;
+    getOptions: () => IConfettiOptions;
+    renderParticle(): void;
+    update(): void;
+}
+export {};
